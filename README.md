3b05f081d0113fadb6ac09441f65a2826a243752
rockney8588@gmail.com <1f289fe4>
2019-02-14 20:58:10 -0700

    Initial commit to GitHub. Tested on Windows x64 and Linux x64.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Stomp
+A file truncation utility. Stomp opens a file, truncates the content,
+then closes the file. Very handy for clearing log files that are
+being monitored.
+
+See the [Stomp Wiki](https://github.com/Corionis/Stomp/wiki) for details and downloads.
---
+++ main.cpp
@@ -0,0 +1,106 @@
+#include <fstream>
+#include <iostream>
+#include <cstring>
+
+/**
+ * stomp - Simple utility to truncate file(s)
+ *
+ * A useful utility for files such as logs that need to exist but be made empty.
+ *
+ * Generally ignores exceptions.
+ *
+ * 1.0 Initial implementation
+ */
+
+char *arg;
+bool quiet = false;
+bool argIs(const char *cmp);
+void parseOptions(int argc, char **argv);
+void stompArguments(int argc, char **argv);
+void usage();
+
+int main(int argc, char* argv[])
+{
+    parseOptions(argc, argv);
+    stompArguments(argc, argv);
+    return 0;
+}
+
+bool argIs(const char *cmp)
+{
+    return strcmp(arg, cmp) == 0;
+}
+
+void parseOptions(int argc, char **argv)
+{
+    // if there are no arguments show usage
+    if (argc < 2) // program path is argv[0]
+    {
+        usage();
+        exit(0);
+    }
+
+    for (int i = 1; i < argc; ++i) // skip program path argv[0]
+    {
+        arg = argv[i];
+        if (arg[0] == '-') // skip non-options
+        {
+            if (argIs("-q"))
+            {
+                quiet = true;
+            }
+            else if (argIs("-?") || argIs("-h") || argIs("--help"))
+            {
+                usage();
+                exit(0);
+            }
+        }
+    }
+}
+
+void stompArguments(int argc, char **argv)
+{
+    int count = 0;
+    for (int i = 1; i < argc; ++i)
+    {
+        char *arg = argv[i];
+        if (arg[0] != '-') // skip options
+        {
+            try
+            {
+                std::fstream fs(arg, std::fstream::out | std::fstream::trunc);
+                ++count;
+            }
+            catch(const std::runtime_error& re)
+            {
+                //std::cerr << "Runtime error: " << re.what() << std::endl;
+            }
+            catch(const std::exception& ex)
+            {
+                //std::cerr << "Error occurred: " << ex.what() << std::endl;
+            }
+            if ( ! quiet)
+            {
+                std::cout << argv[i] << std::endl;
+            }
+        }
+    }
+    if ( ! quiet)
+    {
+        std::cout << "stomped " << count << " files" << std::endl;
+    }
+}
+
+void usage()
+{
+    std::cout << "" << std::endl;
+    std::cout << "stomp v1.0 - Truncate one or more files" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Usage: stomp [-q] file1 file2 ..." << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "  -q  Quiet, do not output anything" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "If a path or filename contains spaces enclose it in double-quotes." << std::endl;
+    std::cout << "" << std::endl;
+}
